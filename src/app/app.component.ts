8b5e2c43e37adddb79621bbea001276974c72113
Yasar <yaltamiranoa@outlook.com>
2020-09-29 10:37:05 -0500

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+
+  nombre = 'Capitán América';
+  nombre2 = 'yAsAr altAMiranO aBASolO';
+  personajes = ['Ironman', 'Spiderman', 'Thor', 'Loki', 'Groot'];
+  PI: number = Math.PI;
+  porcentaje = 0.234;
+  salario = 1234.5;
+  fecha: Date = new Date();
+  activar = true;
+
+  idioma = 'es';
+  videoUrl = 'https://www.youtube.com/embed/hpBBvzrtr60';
+
+  valorPromesa = new Promise<string>( resolve => {
+    setTimeout( () => {
+      resolve('llego la data');
+    }, 4500);
+  });
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Primera',
+      casa: 20
+    }
+  };
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+
+import '@angular/common/locales/global/es';
+import '@angular/common/locales/global/fr';
 
 import { AppComponent } from './app.component';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { ContrasenaPipe } from './pipes/contrasena.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizadoPipe,
+    DomseguroPipe,
+    ContrasenaPipe
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
